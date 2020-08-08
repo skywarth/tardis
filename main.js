d01d5112fb9922c649e6acab22364979dc7bbfde
skywarth <yigitk.ersoy@gmail.com>
2020-08-09 01:01:05 +0300

    css fixes, navbar customization

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $('#goToBottom').click(function(){
 
-    var WH = $(window).height();
+   /* var WH = $(window).height();
     var SH = $('body').prop("scrollHeight");
-    $('html, body').stop().animate({scrollTop: SH-WH}, 1000);
+    $('html, body').stop().animate({scrollTop: SH-WH}, 1000);*/
 
 });
 
